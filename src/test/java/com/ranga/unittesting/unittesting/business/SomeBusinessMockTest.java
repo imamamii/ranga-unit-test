4f6d055f0c01bb083435216df0089e170d3f9860
imamamii <mamigosh@gmail.com>
2021-04-14 17:28:37 -0500

    Mockito resumidoo

--- src/test/java/com/ranga/unittesting/unittesting/business/SomeBusinessMockTest.java
+++ src/test/java/com/ranga/unittesting/unittesting/business/SomeBusinessMockTest.java
@@ -37,38 +37,29 @@ class DataServiceOneValueMock implements DataService{
 
 
 class SomeBusinessMockTest {
+	
+	BusinessImpl business = new BusinessImpl();
+	DataService dataServiceMock = mock(DataService.class);
 
 	@Test
 	public void calculateSumDataService_basic() {
-		BusinessImpl business = new BusinessImpl();
-		DataService dataServiceMock = mock(DataService.class);
 		when(dataServiceMock.retrieveAllData()).thenReturn(new int[] {1,2,3});
 		business.setDataService(dataServiceMock);
-		int actualResult = business.calculateSumDataService();
-		int expectedResult = 6;
-		assertEquals(expectedResult, actualResult);
+		assertEquals(6, business.calculateSumDataService());
 	}
 	
 	@Test
 	public void calculateSumDataService_empty() {
-		BusinessImpl business = new BusinessImpl();
-		DataService dataServiceMock = mock(DataService.class);
 		when(dataServiceMock.retrieveAllData()).thenReturn(new int[] {});
 		business.setDataService(dataServiceMock);
-		int actualResult = business.calculateSumDataService();
-		int expectedResult = 0;
-		assertEquals(expectedResult, actualResult);
+		assertEquals(0, business.calculateSumDataService());
 	}
 	
 	@Test
 	public void calculateSumDataService_oneValue() {
-		BusinessImpl business = new BusinessImpl();
-		DataService dataServiceMock = mock(DataService.class);
-		when(dataServiceMock.retrieveAllData()).thenReturn(new int[] {5});
+		when(dataServiceMock.retrieveAllData()).thenReturn(new int[] {8});
 		business.setDataService(dataServiceMock);
-		int actualResult = business.calculateSumDataService();
-		int expectedResult = 5;
-		assertEquals(expectedResult, actualResult);
+		assertEquals(5, business.calculateSumDataService());
 	}
 	
 
